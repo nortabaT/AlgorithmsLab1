548383bf204275a5f83c4538210b8038425718a2
nortabaT <brandon.dsouza@utexas.edu>
2014-09-30 02:11:49 -0500

    uses arraylists to display all combos

--- BipartiteGraph.java
+++ BipartiteGraph.java
@@ -1,24 +1,22 @@
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
 
 public class BipartiteGraph {
 	
 	public ArrayList<TicNode> tics;
 	public ArrayList<TacNode> tacs;
-	public Set<Set<Edge>> Edges;
+	
+	public ArrayList<ArrayList<Edge>> edges;
 	public BipartiteGraph(ArrayList<TicNode> ticList, ArrayList<TacNode> tacList){
 		tics = ticList;
 		tacs = tacList;
-		Edges = new HashSet<Set<Edge>>();
+		edges = new ArrayList<ArrayList<Edge>>();
 		setUpEdges();
 	}
 	
 	private void setUpEdges(){
 		
 		for(TicNode tic : tics){
-			Set<Edge> possibleMatches = new HashSet<Edge>();
+			ArrayList<Edge> possibleMatches = new ArrayList<Edge>();
 			
 			for(TacNode tac : tacs){
 				if(tac.val >= tic.low && tac.val <= tic.high){
@@ -27,7 +25,27 @@ public class BipartiteGraph {
 			}
 			
 			if(possibleMatches.size() >= 1){
-				Edges.add(possibleMatches);
+				edges.add(possibleMatches);
+			}
+		}
+		for(int i = 0; i < edges.size(); i++){
+			combine(edges, new ArrayList<Edge>(), i, 0);
+		}
+		
+	}
+	
+	private void combine(ArrayList<ArrayList<Edge>> input, ArrayList<Edge> current, int n, int count){
+		
+		if(count == input.size()){
+			for(int i = 0; i < count; i++){
+				System.out.print(current.get(i) + ", ");
+			}
+			System.out.println();
+		}
+		else{
+			for(int i = 0; i < input.get(n).size(); i++){
+				current.add(count, input.get(n).get(i));
+				combine(input, current, (n+1)%(edges.size()), count+1);
 			}
 		}
 	}
